e61a99959acc895ff9696bce3288ab69aa9dd997
Yashveer Singh <y89769879@gmail.com>
2026-08-01 23:16:31 +0530

    Add Number of island.cpp - convert txt to cpp

---
+++ Number of island.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int r, int c, vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        if (r < 0 || c < 0 || r >= n || c >= m || grid[r][c] == '0') {
+            return;
+        }
+        grid[r][c] = '0';
+        dfs(r - 1, c, grid);
+        dfs(r + 1, c, grid);
+        dfs(r, c - 1, grid);
+        dfs(r, c + 1, grid);
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if (grid.empty()) return 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(i, j, grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// Example usage
+int main() {
+    vector<vector<char>> grid = {
+        {'1','1','0','0','0'},
+        {'1','1','0','0','0'},
+        {'0','0','1','0','0'},
+        {'0','0','0','1','1'}
+    };
+    Solution sol;
+    cout << sol.numIslands(grid) << "\n";
+    return 0;
+}
